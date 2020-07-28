8981b9fe8b68bf26daeb23000f5b3dca9684e00c
Grigor Sargsyan <grigor0330@gmail.com>
2020-07-28 19:19:55 +0400

    Added categories routes and controller, changed database name in sequelize config file

---
+++ controllers/categoriesController.js
@@ -0,0 +1,122 @@
+const db = require('../models');
+const Categories = db.categories;
+const sequelize = require('sequelize');
+const {Op} = require('sequelize')
+
+exports.create = async (req, res) => {
+    let data = req.body;
+    let parent = await Categories.findOne({
+        where: {id: data.parent}, attributes: ['id'], include: [
+            {
+                model: Categories,
+                as: 'children'
+            }
+        ]
+    });
+    if (parent && parent.dataValues) {
+
+        data.parent = parent.dataValues.id;
+        data.order = parent.dataValues.children.length;
+        await Categories.create(data);
+        res.json('OK');
+    } else {
+        data.parent = null;
+        data.order = 0;
+        await Categories.create(data);
+        res.json('OK');
+        //     res.status(500).json({msg: 'Parent is not found'});
+    }
+};
+
+exports.get = async (req, res) => {
+
+    const result = await Categories.findAll({
+        where: {
+            parent: null
+        },
+        order: [
+            [sequelize.col(`children.order`), 'asc'],
+            [sequelize.col(`children->children.order`), 'asc'],
+        ],
+        include: [{
+            model: Categories,
+            as: 'children',
+            include: [
+                {
+                    model: Categories,
+                    as: 'children',
+                    include: [
+                        {
+                            model: Categories,
+                            as: 'children',
+                        }
+                    ],
+                }
+            ]
+        }]
+    });
+
+
+    res.json(result);
+};
+
+exports.getCategoriesList = async (req, res) => {
+    const result = await Categories.findAll({
+        where: {
+            parent: {
+                [Op.lt]: 2
+            }
+        }
+    });
+    res.json(result);
+};
+
+exports.getById = async (req, res) => {
+    const result = await Categories.findOne({
+        where: {id: req.query.id}
+    });
+    res.json(result);
+};
+
+
+exports.updateOrder = async (req, res) => {
+    await Categories.destroy({truncate: true, cascade: false});
+    let data = req.body;
+    let result = [];
+    data.map(parentData => {
+        updateParents(parentData, result);
+        parentData.order = +Object.keys(data).find(key => data[key] === parentData);
+        result.push((({children, ...o}) => o)(parentData));
+
+    });
+    console.log(result)
+    await Categories.bulkCreate(result);
+    // let cs = await Categories.findAll({});
+    // res.json(cs)
+    this.get(req, res);
+};
+
+exports.updateDetails = async (req, res) => {
+    let data = req.body;
+    await Categories.update(data, {where: {id: data.id}});
+    res.json('OK');
+};
+
+let updateParents = (parentData, result) => {
+    if (parentData.hasOwnProperty('children') && parentData.children.length > 0) {
+        parentData.children.map(dt => {
+            let key = +Object.keys(parentData.children).find(key => parentData.children[key] === dt);
+            dt.parent = parentData.id;
+            dt.order = key;
+            const clone = (({children, ...o}) => o)(dt);
+            result.push(clone);
+            updateParents(dt, result);
+        })
+    }
+    return parentData;
+};
+
+
+exports.remove = async (req, res) => {
+
+};
---
+++ routes/categories.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const categoriesController = require('../controllers/categoriesController');
+
+router.get('/get', categoriesController.get);
+router.get('/get-list', categoriesController.getCategoriesList);
+router.get('/get-by-id', categoriesController.getById);
+router.post('/create', categoriesController.create);
+router.put('/update-details', categoriesController.updateDetails);
+router.put('/update-order', categoriesController.updateOrder);
+router.remove('/remove', categoriesController.remove);
+
+
+module.exports = router;
